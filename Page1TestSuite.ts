38ecbeebc1982241db7213956ca3192109d19c73
razdanamit51 <63803632+razdanamit51@users.noreply.github.com>
2021-01-27 19:11:45 +0530

    Add files via upload

---
+++ Page1TestSuite.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Page1TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6afb03f8-a853-4f62-93a7-712c31800e15</testSuiteGuid>
+   <testCaseLink>
+      <guid>15e8e4d9-0c77-4b6e-b9b2-d026c09e7f3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyWelcomePageUrl</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5bde7754-6f3c-47d7-9272-154346594322</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyWelcomePageTitle</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9146e5a-239b-46f0-849b-d8d591beef0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyTextBoxesValues</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5c44398-7ef6-4054-a32f-f71ee3c0bf69</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyNameTextboxIsDisplayed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6c9fb64-f083-4935-a225-3e53d78797ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyEmailTextBoxIsDisplayed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>769f1c7f-bc06-4696-98ef-f9fd69e8b3e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifySubmitBtnIsDisplayed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d54ede28-b403-42ef-ae17-0acf20c07253</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyShopOptionIsDisplayed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6de03c21-4cc3-4747-b261-7909f7b6b39c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifyShopOptionWorking</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3c7f5085-5b68-4ed6-a003-bf8735597961</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifySuccessMessageIsDisplayed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f67e9fc0-1a17-4587-bf5b-69ebdb7f6dbd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Page1_TestCases/VerifySuccessMessageValue</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
